c5a1cc38dd3dfef185ccce2b73cd4fea078e6195
Jannis Hermanns <jannis@gmail.com>
2012-08-02 12:50:51 +0200

    Ripped apart enough. Off to adding write support

---
+++ lib/zuck/fb_object/dsl.rb
@@ -0,0 +1,113 @@
+module Zuck
+ module FbObject
+   module DSL
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    # @return [String] Most facebook objects will need to return their
+    #   id property here, so that's the default. Overwrite if necessary
+    def path
+      self[:id]
+    end
+
+    private
+
+    # Sets the parent of this instance
+    #
+    # @param parent [FbObject] Has to be of the same class type you defined
+    #   using {FbObject.parent_object}
+    def set_parent(parent)
+      return unless parent
+      self.class.validate_parent_object_class(parent)
+      @parent_object = parent
+    end
+
+    module ClassMethods
+
+      # Attempts to resolve the {FbObject.parent_object} to a class at runtime
+      # so we can load files in any random order...
+      def resolve_parent_object_class
+        return if @parent_object_class
+        class_s = "Zuck::#{@parent_object_type.camelcase}"
+        @parent_object_class = class_s.constantize
+      end
+
+      # Makes sure the given parent matches what you defined
+      # in {FbObject.parent_object}
+      def validate_parent_object_class(parent)
+        resolve_parent_object_class
+        e = "Invalid parent_object: #{parent.class} is not a #{@parent_object_class}"
+        raise e if @parent_object_class and !parent.is_a?(@parent_object_class)
+      end
+
+
+      # Part of our little DSL, sets the part of the path that fetches the
+      # list of objects from facebook.
+      #
+      #     class Foo < FbObject
+      #        ...
+      #        list_path :foos
+      #      end
+      #
+      # {FbObject} uses this to construct a path together with this class'
+      # parent object's path method (which is usually just it's ID
+      # property)
+      #
+      # @param path [String, Symbol] Pass a value if you want to set the
+      #   list_path for this object.
+      # @return The object's `list_path`
+      def list_path(path = nil)
+        @list_path = path if path
+        @list_path
+      end
+
+      # Pretty much like a `belongs_to`, but is used to construct paths to
+      # access the facebook api.
+      #
+      # It also defines a getter method. Look
+      #
+      #     class AdCampaign < FbObject
+      #       ...
+      #       parent_object :ad_account
+      #     end
+      #
+      # Now on instances you can call `my_campaign.ad_account` to fetch
+      # the ad account your campaign is part of.
+      #
+      # @param type [Symbol] Pass an underscored symbol here, for example
+      #   `ad_account`
+      def parent_object(type)
+        @parent_object_type = type.to_s
+        define_method(type) do
+          @parent_object
+        end
+      end
+
+      # Defines which other classes might have this one as their parent.
+      #
+      # If you do something like
+      #
+      # ```ruby
+      # class Foo < RawFbObject
+      #   ...
+      #   connections :dings, :dongs
+      # end
+      # ```
+      #
+      # then your `Foo` instances will have a `#dings` and `#dongs` methods,
+      # which will call `Ding.all` and `Dong.call` on the appropriate graph
+      # object.
+      def connections(*args)
+        args.each do |c|
+          define_method(c.to_s.pluralize) do
+            clazz = "Zuck::#{c.to_s.singularize.camelize}".constantize
+            clazz.all(graph, self)
+          end
+        end
+      end
+    end
+   end
+ end
+end
+
--- lib/zuck/fb_object/read.rb
+++ lib/zuck/fb_object/read.rb
@@ -5,7 +5,50 @@ module Zuck
       base.extend(ClassMethods)
     end
 
+    # @param graph [Koala::Facebook::API] A graph with access_token
+    # @param data [Hash] The properties you want to assign, this is what
+    #   facebook gave us (see known_keys).
+    # @param parent [<FbObject] A parent context for this class, must
+    #   inherit from {Zuck::FbObject}
+    def initialize(graph, data, parent=nil)
+      self.graph = graph
+      set_hash_delegator_data(data)
+      set_parent(parent)
+    end
+
     module ClassMethods
+
+      # Automatique all getter.
+      #
+      # Let's say you want to fetch all campaigns
+      # from facebook. This can only happen in the context of an ad
+      # account. In this gem, that context is called a parent. This method
+      # would only be called on objects that inherit from {FbObject}.
+      # It asks the `parent` for it's path (if it is given), and appends
+      # it's own `list_path` property that you have defined (see
+      # list_path)
+      #
+      # @param graph [Koala::Facebook::API] A graph with access_token
+      # @param parent [<FbObject] A parent object (needed always, except
+      #   when you fetch a {Zuck::AdAccount}
+      def all(graph, parent = nil)
+        paths = []
+        paths << parent.path if parent
+        paths << list_path
+        r = get(graph, paths.join('/'))
+        r.map do |c|
+          new(graph, c, parent)
+        end
+      end
+
+      private
+
+      # Just a helper for debugging and what not
+      def get(graph, path)
+        # puts "Fetching #{path}"
+        graph.get_object(path)
+      end
+
     end
    end
  end
