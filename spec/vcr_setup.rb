c9cc2dccaa2ee531c76654c1d8b9f4c5b6456a93
Jannis Hermanns <jannis@gmail.com>
2012-08-21 15:45:54 +0200

    Makes vcr options stricter and adds logging

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -1,11 +1,15 @@
 # VCR Config
 require 'vcr'
-
+FileUtils.mkdir("log") rescue nil
 VCR.configure do |c|
   dir = File.expand_path("../../spec/fixtures", __FILE__)
   c.cassette_library_dir      = dir
   c.hook_into                 :webmock
   c.ignore_localhost          = true
-  c.default_cassette_options  = { :record => :new_episodes }
+  c.default_cassette_options  = {
+ #   record: :new_episodes
+    match_requests_on: [:method, :body, :uri]
+  }
   c.allow_http_connections_when_no_cassette = false
+  c.debug_logger = File.open(File.expand_path("../../log/vcr.log", __FILE__), "w")
 end
