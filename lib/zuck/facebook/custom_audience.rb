1d13bed0321acead1d6fddd7a2cd0994808ceaf3
martin sarsale <martin@properati.com>
2014-06-10 16:38:38 -0300

    CustomAudiences and Emails

--- lib/zuck/facebook/custom_audience.rb
+++ lib/zuck/facebook/custom_audience.rb
@@ -1,4 +1,5 @@
 module Zuck
+  require "digest/md5"
   class CustomAudience < RawFbObject
 
     # The [fb docs](https://developers.facebook.com/docs/reference/ads-api/adaccount/)
@@ -16,6 +17,18 @@ module Zuck
 
     parent_object :ad_account
     list_path     :customaudiences
+
+
+    def emails=(emails)
+      audience = emails.map{|email|
+        {email_hash: Digest::MD5.hexdigest(email)}
+      }
+      add_users(audience, "md5")
+    end
+
+    def add_users(audience, hash_type)
+      create_connection(graph, self.id, :users, {users: audience.to_json, hash_type: hash_type})
+    end
 
   end
 end
