17332040be1dce118f2de81863f6f74c766adb39
cte <cestreich@gmail.com>
2016-04-30 18:35:32 -0700

    Fix gem.

--- lib/zuck.rb
+++ lib/zuck.rb
@@ -1,10 +1,15 @@
+# External requires.
 require 'active_support/all'
+require 'koala'
+
+# Internal requires.
 require 'zuck/koala/koala_methods'
-Koala.config.api_version = 'v2.6'
-Dir[File.expand_path("../zuck/fb_object/**/*.rb", __FILE__)].each { |f| require f }
+Dir[File.expand_path('../zuck/fb_object/**/*.rb', __FILE__)].each { |f| require f }
 require 'zuck/fb_object'
 Zuck::RawFbObject = Zuck::FbObject::RawFbObject
 Dir[File.expand_path('../zuck/facebook/**/*.rb', __FILE__)].each { |f| require f }
+
+Koala.config.api_version = 'v2.6'
 
 module Zuck
   extend KoalaMethods
